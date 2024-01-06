e81c04a83368ce1fe3ddb142f523dca684f5891e
Aishik Barua <aishikbarua07@gmail.com>
2024-01-06 09:22:56 +0600

    feature,possibility,whatgtp3 sections

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -7,6 +7,7 @@ const Menu= () => (
     <>
       <p><a href="#home">Home</a></p>
       <p><a href="#wgpt3">What is GPT3?</a></p>
+      <p><a href="#possibility">Open AI</a></p>
       <p><a href="#features">Case Studies</a></p>
       <p><a href="#blog">Library</a></p>
   </>
@@ -34,7 +35,7 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+export default Navbar;
 
 /*<div className="gpt3__navbar-menu">
             {toggleMenu
